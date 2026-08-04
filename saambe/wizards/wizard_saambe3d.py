712b740e92118bf5a0d04f3d75ef42135600e0e7
judith-MC <judith3atk@gmail.com>
2026-08-04 11:31:39 +0200

    fix: sonarCloud issue

--- saambe/wizards/wizard_saambe3d.py
+++ saambe/wizards/wizard_saambe3d.py
@@ -147,7 +147,7 @@ class AddMutationsSaambe(EmWizard):
         mutations = self.getMutations(form)
 
         toMutateList = protocol.toMutateList.get()
-        existing = set(line.strip() for line in toMutateList.strip().split("\n") if line.strip())
+        existing = {line.strip() for line in toMutateList.strip().split("\n") if line.strip()}
         newMutations = [m for m in mutations if m not in existing]
         toMutateList += "\n" + "\n".join(newMutations)
         form.setVar('toMutateList', toMutateList.strip())
